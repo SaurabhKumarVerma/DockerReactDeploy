11a0893def84bfa43fadd7055232cdde85503785
Saurabh Kumar Verma <saurav88871kumar@hotmail.com>
2022-01-30 15:52:25 +0530

    Test Cases Change

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('list contains 5 animals', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const listElement = screen.getByRole('list');
+  const listItems = screen.getAllByRole('listitem');
+
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toHaveClass('animals');
+  expect(listItems.length).toEqual(5);
 });
